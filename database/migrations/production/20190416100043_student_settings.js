aa5917efe88ac4cf6af2f6d18c6a4162fe07be0b
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-16 11:52:26 -0500

    Added student settings faker data

--- database/migrations/production/20190416100043_student_settings.js
+++ database/migrations/production/20190416100043_student_settings.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('student_settings', (tbl) => {
         tbl.integer('student_id').unsigned().references('id').inTable('student_info');
         tbl.integer('level').notNullable();
-        tbl.boolean('spanish_type').notNullable();
+        tbl.boolean('latin_spanish').notNullable();
     });
   };
   
