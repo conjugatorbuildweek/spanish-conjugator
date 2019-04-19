0d244bc749d03f0b82f5bb3b8c8a2a197f41fb64
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-19 00:02:25 -0400

    cleanup

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './server/database/dev.sqlite3'
     },
     migrations: {
       directory: './server/database/migrations/production'
